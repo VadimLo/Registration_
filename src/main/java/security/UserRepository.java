5d8c91944ae003070c3c4125516a62997ef42d04
VadimLo <41148811+VadimLo@users.noreply.github.com>
2019-06-16 16:26:13 +0300

    initial commit

---
+++ src/main/java/security/UserRepository.java
@@ -0,0 +1,12 @@
+package security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+   User findByname(String name);
+
+}
