d7298d91ae0d2a73da32f8bf76b0c1da9affbb75
VadimLo <41148811+VadimLo@users.noreply.github.com>
2019-07-15 15:32:42 +0300

    add user role edit

--- src/main/java/security/repositories/UserRepository.java
+++ src/main/java/security/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import security.entities.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Long> {
    User findByLogin(String name);
 
 }
